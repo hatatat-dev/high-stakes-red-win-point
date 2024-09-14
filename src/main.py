1a1495e0c31cebfc5a992e097f32e6a1a7969f49
hatatat-dev <hatatat@glowstonemania.com>
2024-09-14 10:38:35 -0700

    [autonomous] autonomous drives forward 1 meter

--- src/main.py
+++ src/main.py
@@ -3,7 +3,19 @@
 from lib.log import *
 from high_stakes.events import *
 
-open_log("manual.csv")
+open_log("red_winpoint.csv")
+
+
+def driver_function():
+    pass
+
+
+def autonomous_function():
+    pid_driver.drive(1000)
+    pass
 
 
 init_event_handling()
+
+# register the competition functions
+competition = Competition(driver_function, autonomous_function)
