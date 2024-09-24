8d4ce9ebf91ba5fa59fbbff06e7affa2b058f6f7
hatatat-dev <hatatat@glowstonemania.com>
2024-09-23 19:39:39 -0700

    [telemetry] rename lib to telemetry

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S PYTHONPATH=../telemetry python3
 
-from lib.log import *
+from telemetry.log import *
 from high_stakes.events import *
 
-open_log("red-win-point.csv")
+open_log("high-stakes-red-win-point.csv")
 
 
 def driver_function():
